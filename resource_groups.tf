bae643602eda838aac3eb1b47137bae5af0a2448
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2024-04-09 21:13:00 +0200

    Rename resource group (#2)

--- resource_groups.tf
+++ resource_groups.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "prjo_ne_001" {
-  name     = "${local.resource_type.resource_group}-${local.project_name}-${terraform.workspace}-001"
+  name     = "${local.resource_type.resource_group}-${local.project_name}-${terraform.workspace}-${local.location.north_europe.geo_code}-001"
   location = local.location.north_europe.name
 }
