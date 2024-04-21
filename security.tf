61041f893e5c0ea9fd3c57ad65d36cc9df3e014d
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2024-04-21 10:06:28 +0000

    Update README

--- security.tf
+++ security.tf
@@ -1,5 +1,5 @@
-resource "azurerm_role_assignment" "current_service_principal_with_key_vault_administrator_in_kv_prjo_ne_001" {
-  scope                = azurerm_key_vault.prjo_ne_001.id
-  role_definition_name = "Key Vault Administrator"
-  principal_id         = data.azuread_service_principal.current.object_id
-}
+# resource "azurerm_role_assignment" "current_service_principal_with_key_vault_administrator_in_kv_prjo_ne_001" {
+#   scope                = azurerm_key_vault.prjo_ne_001.id
+#   role_definition_name = "Key Vault Administrator"
+#   principal_id         = data.azuread_service_principal.current.object_id
+# }
