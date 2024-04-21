9bd2afb02a69c491c262090c9e93a21f88238eb9
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2024-04-21 08:04:16 +0000

    Update README

--- security.tf
+++ security.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "current_identity_with_key_vault_administrator" {
   scope                = azurerm_key_vault.prjo_ne_001.id
   role_definition_name = "Key Vault Administrator"
-  principal_id         = data.azurerm_client_config.current.principal_id
+  principal_id         = data.azurerm_client_config.current.client_id
 }
