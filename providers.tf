c121e51e23cdd1bceeba0a073e5c9613e7f61a9b
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2024-05-14 15:33:23 +0000

    Update README

--- providers.tf
+++ providers.tf
@@ -11,12 +11,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 3.102.0"
+      version = "= 3.103.1"
     }
 
     azuread = {
       source  = "hashicorp/azuread"
-      version = "= 2.48.0"
+      version = "= 2.49.0"
     }
   }
 }
